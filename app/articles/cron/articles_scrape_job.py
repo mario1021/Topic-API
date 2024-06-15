85d4cb12f1fa11bdcd24706b371e4d3de5575780
Mario Díaz <mario1021br@gmail.com>
2024-06-15 14:43:38 +0200

    change of translation mechanism

--- app/articles/cron/articles_scrape_job.py
+++ app/articles/cron/articles_scrape_job.py
@@ -1,22 +1,9 @@
 import newspaper
 from deep_translator import GoogleTranslator
-import re
 from ..models.source_model import Source
 from ..models.article_model import Article
 import datetime
 
-def split_text(text):
-    split_text = re.split(r'(?<=[.!?]) +', text)
-    return split_text
-
-def translate(text):
-    translated_text=[]
-    for sentence in split_text(text):
-        translation = GoogleTranslator(source='es', target='en').translate(sentence)
-        translated_text.append(translation)
-
-    translated_text=' '.join(translated_text)
-    return translated_text
 
 def scrape_articles():
     articles_list = []
@@ -33,11 +20,10 @@ def scrape_articles():
                 if (datetime.datetime.now() - article.publish_date).days > 2:
                     continue
             if article.text:
-                translated_text = translate(article.text)
                 articles_list.append({
                     "title": article.title,
-                    "text": translated_text,
-                    "full_text" : f"Title: {article.title}\nText: {article.text}\nKeywords: {(' ').join(article.keywords)}",
+                    "text": article.text,
+                    "full_text" : f"Title: {article.title}. \nText: {article.text}. \nKeywords: {(' ').join(article.keywords)}.",
                     "url": article.url,
                     "source_id": source.id,
                     "keywords": article.keywords,
--- app/articles/cron/zero_shot_job.py
+++ app/articles/cron/zero_shot_job.py
@@ -1,6 +1,13 @@
 from transformers import pipeline
 from ...topics.models.topic_model import Topic
 from ..models.article_model import Article
+import re
+
+def split_text(text):
+    split_text = re.split(r'(?<=[.!?]) +', text)
+    return split_text
+
+
 
 def classificate_articles(articles_list):
     zeroshot_classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
@@ -9,7 +16,18 @@ def classificate_articles(articles_list):
     topic_titles = [topic.title for topic in Topic.get_all()]
     topics=Topic.get_all()
 
+    translate_pipeline = pipeline("translation", model="Helsinki-NLP/opus-mt-es-en")
+    for topic in topic_titles:
+        topic = translate_pipeline(topic, target_language="es")[0]['translation_text']
+
+
     for article in articles_list:
+        #to translate the article, we split it into sentences and translate each one, then we join them back together
+        senteces = split_text(article['full_text'])
+        translated_text = ""
+        for sentence in senteces:
+            translated_text += translate_pipeline(sentence, target_language="en")[0]['translation_text'] + " "
+        article['full_text'] = translated_text
         classification=zeroshot_classifier(article['full_text'], candidate_labels=topic_titles, multi_label=True, hypothesis_template=hypothesis_template)
         #labels=[label for label in classification['labels'] if classification['scores'][classification['labels'].index(label)]>threshold]
         labels=[label for label in classification['labels'] if classification['scores'][classification['labels'].index(label)]>threshold]
